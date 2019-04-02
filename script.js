f7e365054b153efb09b7bec0532d14a4620a9c0b
rursteve2 <47332766+rursteve2@users.noreply.github.com>
2019-04-02 17:35:45 -0400

    Added some functionality, made dot jump locations

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let gameSpace = document.querySelector(".gamespace")
 let timeLeft = document.querySelector("#timeleft")
 let score = document.querySelector("#score")
 let makeDot = document.createElement("div")
-let interval = 2000;
+let interval = 1000;
 let button = document.getElementsByClassName("button")
 function createDot() {
 	gameSpace.appendChild(makeDot)
@@ -19,12 +19,15 @@ function respawnDot() {
 function addScore() {
 	let dot = document.querySelector(".dot")
 	score.innerHTML = 0
-	dot.addEventListener('click', function(){
-		dot.style.left = `${Math.floor(Math.random() * gameSpace.clientWidth)}px`
-		dot.style.top = `${Math.floor(Math.random() * gameSpace.clientHeight)}px`
-		score.innerHTML = ++score.innerHTML
+	gameSpace.addEventListener('click', function(e) {
+		if (e.target == dot) {
+			dot.style.left = `${Math.floor(Math.random() * gameSpace.clientWidth)}px`
+			dot.style.top = `${Math.floor(Math.random() * gameSpace.clientHeight)}px`
+			score.innerHTML = 1 + parseInt(score.innerHTML)
+		} else {
+			subtractScore()
+		}
 	})
-	// gameSpace.addEventListener('click', )
 }
 
 function timer() {
@@ -34,51 +37,57 @@ function timer() {
 		if (timeLeft.innerHTML == 0) {
 			clearInterval(time)
 			alert("Finished!")
-			// alert("Finished!")
 		}
-
 	}, 100)
 }
 
+function subtractScore() {
+	score.innerHTML = score.innerHTML - .5
+}
 
 function easyDifficulty() {
+	let dot = document.querySelector(".dot")
+	dot.style.width = '50px'
+	dot.style.height = '50px'
+}
+function normalDifficulty() {
+	let dot = document.querySelector(".dot")
 	dot.style.width = '30px'
 	dot.style.height = '30px'
 }
-function normalDifficulty() {
-	dot.style.width = '20px'
-	dot.style.height = '20px'
-}
 function hardDifficulty() {
+	let dot = document.querySelector(".dot")
 	dot.style.width = '15px'
 	dot.style.height = '15px'
 }
 function extremeDifficulty() {
+	let dot = document.querySelector(".dot")
 	dot.style.width = '15px'
 	dot.style.height = '15px'
 }
 function setDifficulty() {
-	button[0].addEventListener('click', easyDifficulty)
-	button[1].addEventListener('click', normalDifficulty)
-	button[2].addEventListener('click', hardDifficulty)
-	button[3].addEventListener('click', extremeDifficulty)
+	document.getElementById('easybutton').addEventListener('click', easyDifficulty)
+	document.getElementById('normalbutton').addEventListener('click', normalDifficulty)
+	document.getElementById('hardbutton').addEventListener('click', hardDifficulty)
+	document.getElementById('extremebutton').addEventListener('click', extremeDifficulty)
 }
-// let timeLeft = 10
-// var downloadTimer = setInterval(function(){
-//   document.getElementById("timeleft").innerHTML = timeLeft + " seconds remaining";
-//   timeLeft -= 1;
-//   if(timeLeft < 0){
-//     clearInterval(downloadTimer);
-//     document.getElementById("timeleft").innerHTML = "Finished"
-//   }
-// }, 1000);
+function dotDisappear() {
+	console.log(timeLeft);
+	if (timeLeft.innerText > 0) {
+		let dotSpawn = setInterval(respawnDot, interval)
+		
+	} else {
+		clearTimeout(dotSpawn)
+	}
+}
 
 function playGame() {
-createDot()
-respawnDot()
-setDifficulty()
-addScore()
-timer()
+	createDot()
+	setDifficulty()
+	timer()
+	dotDisappear()
+	addScore()
+
 }
 playGame()
 // let play = window.setTimeout(playGame(), 3000)
