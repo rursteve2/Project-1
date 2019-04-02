e0b4d8379d6d8e9c92cb0ad8aa5e23f70d6a42ba
rursteve2 <47332766+rursteve2@users.noreply.github.com>
2019-04-02 11:53:11 -0400

    Created game, buttons/timeout not working

--- script.js
+++ script.js
@@ -4,20 +4,27 @@ let gameSpace = document.querySelector(".gamespace")
 let timeLeft = document.querySelector("#timeleft")
 let score = document.querySelector("#score")
 let makeDot = document.createElement("div")
-let dot = document.querySelector(".dot")
 let interval = 2000;
+let button = document.getElementsByClassName("button")
 function createDot() {
 	gameSpace.appendChild(makeDot)
 	makeDot.classList.add("dot")
 }
+
 function respawnDot() {
-	dot.style.left = (Math.random() * gameSpace.innerWidth) + 'px'
-	dot.style.top = (Math.random() * gameSpace.innerHeight) + 'px'
+	let dot = document.querySelector(".dot")
+	dot.style.left = `${Math.floor(Math.random() * gameSpace.clientWidth)}px`
+	dot.style.top = `${Math.floor(Math.random() * gameSpace.clientHeight)}px`
 }
 function addScore() {
+	let dot = document.querySelector(".dot")
+	score.innerHTML = 0
 	dot.addEventListener('click', function(){
-		score += 1
+		dot.style.left = `${Math.floor(Math.random() * gameSpace.clientWidth)}px`
+		dot.style.top = `${Math.floor(Math.random() * gameSpace.clientHeight)}px`
+		score.innerHTML = ++score.innerHTML
 	})
+	// gameSpace.addEventListener('click', )
 }
 
 function timer() {
@@ -33,6 +40,29 @@ function timer() {
 	}, 100)
 }
 
+
+function easyDifficulty() {
+	dot.style.width = '30px'
+	dot.style.height = '30px'
+}
+function normalDifficulty() {
+	dot.style.width = '20px'
+	dot.style.height = '20px'
+}
+function hardDifficulty() {
+	dot.style.width = '15px'
+	dot.style.height = '15px'
+}
+function extremeDifficulty() {
+	dot.style.width = '15px'
+	dot.style.height = '15px'
+}
+function setDifficulty() {
+	button[0].addEventListener('click', easyDifficulty)
+	button[1].addEventListener('click', normalDifficulty)
+	button[2].addEventListener('click', hardDifficulty)
+	button[3].addEventListener('click', extremeDifficulty)
+}
 // let timeLeft = 10
 // var downloadTimer = setInterval(function(){
 //   document.getElementById("timeleft").innerHTML = timeLeft + " seconds remaining";
@@ -46,7 +76,10 @@ function timer() {
 function playGame() {
 createDot()
 respawnDot()
+setDifficulty()
 addScore()
 timer()
 }
 playGame()
+// let play = window.setTimeout(playGame(), 3000)
+// window.clearTimeout(play)
